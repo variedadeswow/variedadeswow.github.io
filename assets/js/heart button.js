52d0d95c99b32eb58d5619e94119793944b41d00
Luisa Juliana <88605591+Luanavasro@users.noreply.github.com>
2021-09-02 07:01:50 -0500

    Update heart button.js

--- assets/js/heart button.js
+++ assets/js/heart button.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $("#heart").click(function(){
     if($("#heart").hasClass("liked")){
-      $("#heart").html('<a class="icon solid fa-heart" aria-hidden="true"></a>');
+      $("#heart").html('<i class="icon solid fa-heart" aria-hidden="true"></i>');
       $("#heart").removeClass("liked");
     }else{
-      $("#heart").html('<a class="icon solid fa-heart" aria-hidden="true"></a>');
+      $("#heart").html('<i class="icon solid fa-heart" aria-hidden="true"></i>');
       $("#heart").addClass("liked");
           }
   });
